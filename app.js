7021f18ae67cfb55c35f1ebf4e0ecef5719188ae
Rinat Tuhvatshin <rinat@kloop.kg>
2016-12-08 21:31:01 +0000

    Add char calculator functionality

--- app.js
+++ app.js
@@ -4,8 +4,21 @@
     angular.module('myFirstApp', [])
     
     .controller('MyFirstController', function ($scope) {
-        $scope.name = "Rinat";
+        $scope.name = "";
+        $scope.numericValue = 0;
         
+        $scope.displayNumericValue = function () {
+            $scope.numericValue = calculateNumericValue($scope.name);
+        };
+        
+        function calculateNumericValue (string) {
+            var numericValue = 0;
+            for (var i = 0; i < string.length; i++) {
+                numericValue += string.charCodeAt(i);
+            };
+            $scope.debugNumericValue = "calculateNumericValue";
+            return numericValue;
+        };
     });
     
 })();
